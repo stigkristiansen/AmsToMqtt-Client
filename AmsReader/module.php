a29d7d0217c4f91896cd99ec9c29bb3373c68939
stigkristiansen <stigkristiansen@outlook.com>
2021-11-24 13:16:28 +0100

    .

--- AmsReader/module.php
+++ AmsReader/module.php
@@ -3,21 +3,30 @@
 declare(strict_types=1);
 	class AmsReader extends IPSModule
 	{
-		public function Create()
-		{
+		public function Create() {
 			//Never delete this line!
 			parent::Create();
+
+			$this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+
+			$this->RegisterPropertyString('MQTTTopic', '');
 		}
 
-		public function Destroy()
-		{
+		public function Destroy() {
 			//Never delete this line!
 			parent::Destroy();
 		}
 
-		public function ApplyChanges()
-		{
+		public function ApplyChanges() {
 			//Never delete this line!
 			parent::ApplyChanges();
+
+			$this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+						
+			$this->SetReceiveDataFilter('.*' . $this->ReadPropertyString('MQTTTopic') . '".*');
+		}
+
+		public function ReceiveData($JSONString) {
+        	$this->SendDebug(__FUNCTION__, $JSONString, 0);
 		}
 	}
